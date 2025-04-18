45d38d866825db1522cc06c7df3a07c3d163743b
박민건 <min@MacBook-Air.local>
2025-04-19 00:30:49 +0900

    Fix import paths and dependencies for Streamlit Share

--- run_scienceqa/run.py
+++ run_scienceqa/run.py
@@ -10,7 +10,9 @@ except ImportError:
         return iterable
 
 # add the parent directory to the path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 from utilities import *
 from model import solver
